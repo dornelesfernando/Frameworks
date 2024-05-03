cb421fb0bf0e493243308988e2cd069eb66a139e
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-05-03 00:29:15 -0300

    inicializate project

--- React.js/costs/src/App.js
+++ React.js/costs/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/company">Company</Link>
+          <Link to="/newproject">New Project</Link>
+        </div>
+        <Routes>
+          <Route exact path='/' element={<Container customClass="min-height"><Home /></Container>}/>
+          <Route exact path='/contact' element={<Container><Contact /></Container>}/>
+          <Route exact path='/company' element={<Container><Company /></Container>}/>
+          <Route exact path='/newproject' element={<Container><NewProject /></Container>}/>
+        </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ React.js/costs/src/components/pages/NewProject.js
@@ -0,0 +1,10 @@
+function NewProject(){
+    return(
+        <div>
+            <h1>New Project</h1>
+            <p>Conteúdo da página</p>
+        </div>
+    )
+}
+
+export default NewProject;
