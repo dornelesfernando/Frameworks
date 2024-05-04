578cd8afaabb0d5eaade8103508112027f433458
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-05-04 00:50:14 -0300

    Navbar and Footer add

--- React.js/costs/src/App.js
+++ React.js/costs/src/App.js
@@ -1,27 +1,26 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/company">Company</Link>
-          <Link to="/newproject">New Project</Link>
-        </div>
+        <NavBar />
         <Routes>
           <Route exact path='/' element={<Container customClass="min-height"><Home /></Container>}/>
-          <Route exact path='/contact' element={<Container><Contact /></Container>}/>
-          <Route exact path='/company' element={<Container><Company /></Container>}/>
-          <Route exact path='/newproject' element={<Container><NewProject /></Container>}/>
+          <Route exact path='/projects' element={<Container customClass="min-height"><Projects /></Container>}/>
+          <Route exact path='/contact' element={<Container customClass="min-height"><Contact /></Container>}/>
+          <Route exact path='/company' element={<Container customClass="min-height"><Company /></Container>}/>
+          <Route exact path='/newproject' element={<Container customClass="min-height"><NewProject /></Container>}/>
         </Routes>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
---
+++ React.js/costs/src/components/pages/Projects.js
@@ -0,0 +1,10 @@
+function Projects(){
+    return(
+        <div>
+            <h1>Projects</h1>
+            <p>Conteúdo da página</p>
+        </div>
+    )
+}
+
+export default Projects;
