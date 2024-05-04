4772e175a19bd8cd14fb42a50cdb6c21902474a8
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-05-04 01:53:51 -0300

    Construct NewProject Page

--- React.js/costs/src/components/pages/NewProject.js
+++ React.js/costs/src/components/pages/NewProject.js
@@ -1,8 +1,12 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject(){
     return(
-        <div>
-            <h1>New Project</h1>
-            <p>Conteúdo da página</p>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
         </div>
     )
 }
