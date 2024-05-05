d4b6a59d73a989a43bea47d2b38fcc2e5a0e5184
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-05-05 03:42:46 -0300

    Construct Project page

--- React.js/costs/src/App.js
+++ React.js/costs/src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import NavBar from './components/layout/NavBar';
@@ -15,10 +16,11 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path='/' element={<Container customClass="min-height"><Home /></Container>}/>
-          <Route exact path='/projects' element={<Container customClass="min-height"><Projects /></Container>}/>
-          <Route exact path='/contact' element={<Container customClass="min-height"><Contact /></Container>}/>
-          <Route exact path='/company' element={<Container customClass="min-height"><Company /></Container>}/>
-          <Route exact path='/newproject' element={<Container customClass="min-height"><NewProject /></Container>}/>
+          <Route path='/projects' element={<Container customClass="min-height"><Projects /></Container>}/>
+          <Route path='/contact' element={<Container customClass="min-height"><Contact /></Container>}/>
+          <Route path='/company' element={<Container customClass="min-height"><Company /></Container>}/>
+          <Route path='/newproject' element={<Container customClass="min-height"><NewProject /></Container>}/>
+          <Route path='/project/:id' element={<Container customClass="min-height"><Project /></Container>}/>
         </Routes>
       <Footer />
     </Router>
