260e55673ab1736a1e621e085fcd9f6111bb1489
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-05-05 01:00:01 -0300

    Dashboard and Message
    Problem: Message sending
    
    Component Message (layout/Message) does not receive parameter
    
    pages/NewProject: 24
    pages/Projects: 22

--- React.js/costs/src/components/pages/NewProject.js
+++ React.js/costs/src/components/pages/NewProject.js
@@ -1,42 +1,37 @@
 import { useNavigate } from 'react-router-dom'
 
-import ProjectForm from '../project/ProjectForm';
+import ProjectForm from '../project/ProjectForm'
+
 import styles from './NewProject.module.css'
 
-function NewProject(){
+function NewProject() {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate();
+  function createPost(project) {
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
 
-    function createPost(project) {
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        navigate('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+  }
 
-        // Initialize coit and services
-
-        project.cost = 0;
-        project.services = []
-
-        const url = "http://localhost:5000/projects";
-        fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(project),
-        })
-        .then((resp) => resp.json)
-        .then((data) => {
-            console.log(data)
-            navigate('/projects', {message: 'Projeto criado com sucesso!'})
-        })
-        .catch((err) => console.log(err))
-    }
-
-    return(
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
-        </div>
-    )
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  )
 }
 
-export default NewProject;
+export default NewProject
